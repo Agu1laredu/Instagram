f6dec000ce4566cacf18581b1a99317d341fe2a9
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-02-28 01:46:01 -0300

    corrigiendo css

--- src/Components/PhotoCard/styles.jsx
+++ src/Components/PhotoCard/styles.jsx
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 import{fadeIn}from '../../styles/animation'
+
+
+export const Article = styled.article`
+  min-height: 200px;
+`
+
 export const ImgWrapper=styled.div`
   display: block;
   height: 0;
